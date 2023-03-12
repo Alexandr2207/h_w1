b343e534fd0be054972bfa7efca4a5733345d240
Alexandr2207 <123030979+Alexandr2207@users.noreply.github.com>
2023-03-12 20:52:32 +0200

    upd

--- hw2/clean_folder/setup.py
+++ hw2/clean_folder/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='clean_folder',
     version='1.0',
+    description='Useful programm to clean and sort your files and folders',
+    author = 'Oleksandr Frolov',
+    author_email='alexandrgreat07@gmail.com',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean_folder = clean_folder.clean:main']},
 
